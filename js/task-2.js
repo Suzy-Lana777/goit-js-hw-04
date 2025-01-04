658c0df8f43aae8ea70f4719223c5b8c2cf6030e
Suzy-Lana777 <133979206+Suzy-Lana777@users.noreply.github.com>
2025-01-04 17:26:33 +0200

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,14 @@
+function makeArray(firstArray, secondArray, maxLength) {
+  let newmakeArray = firstArray.concat(secondArray);
+  if (newmakeArray.length > maxLength) {
+    return newmakeArray.slice(0, maxLength);
+  }
+  return newmakeArray;
+}
+
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
